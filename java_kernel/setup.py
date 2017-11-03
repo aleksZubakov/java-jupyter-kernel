2539079bac8f0b8bf021d6990ef956fb1145f27c
Anton <anton1321@mail.ru>
2017-11-03 20:10:41 +0300

    setup.py add. Use "./setup.py install" .

---
+++ java_kernel/setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+from setuptools import setup, find_packages
+
+import subprocess
+
+if __name__ == "__main__":
+    setup(name="javaForJupyter",
+          author="Devdays",
+          description="A Juyter kernel for Java.",
+          
+          python_requires=">=3.5",
+          install_requires=[
+              "py4j",
+          ],
+          data_files=[('/usr/local/share/jupyter/kernels/java', ['kernel.json'])]
+ 	)
+    subprocess.call("mvn clean compile assembly:single",shell = True)
+    print("Setup, maybe, done")
