cef57ffa9c14a58b21f40a16bc5e5160ac1d24e1
Anton <anton1321@mail.ru>
2017-11-05 20:41:47 +0300

    in setup.py message added.

--- java_kernel/setup.py
+++ java_kernel/setup.py
@@ -6,28 +6,32 @@ import subprocess, re, os
 
 if __name__ == "__main__":
     #subprocess.call("mvn clean install",shell = True)
-    
+
     pth_re = re.compile(r'\S*jupyter/kernels/')
-    path = pth_re.search(str(subprocess.check_output(['jupyter', 'kernelspec', 'list']))).group() + "java/"
-    
+
+    try:
+        path = pth_re.search(str(subprocess.check_output(['jupyter', 'kernelspec', 'list']))).group() + "java/"
+    except Exception:
+        print("You should have installed Jupyter with some kernels.")
+        quit()
     if not os.path.exists(path):
     	os.makedirs(path)
-    
+
     f = open(path+'kernel.json', 'w')
-    
+
     f.write('{"argv":["python","'+path+'kernel.py", "'+path+'jserver.jar", "-f", "{connection_file}"],"display_name":"Java"}\n')
     f.close()
-    
+
     setup(name="javaForJupyter",
           author="Devdays",
           description="A Jupyter kernel for Java.",
-          
+
           python_requires=">=3.5",
           install_requires=[
               "py4j",
           ],
           data_files=[(path, ['kernel.py','target/jserver.jar'])]
  	)
-    
+
     print("Setup, maybe, done in directory\n" + path)
     print("To launch Jupyter with Java kernel, type 'jupyter console --kernel java'")
